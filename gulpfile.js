cd2c3b9e4ebbf1f05bfa411f6ab5c4c3b23cebfb
Jorg Are <jorg2all@yahoo.com>
2016-12-08 12:40:42 +0100

    fix(houndci): fix houndci issues

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ gulp.task('browserSyn', () => {
   browserSync.init({
     server: {
       baseDir: './',
-        index: 'index.html'
-      },
-      port: process.env.PORT || 3000,
-      open: false
+      index: 'index.html'
+    },
+    port: process.env.PORT || 3000,
+    open: false
   });
 });
-gulp.task('app',['browserSyn'], () => {
+gulp.task('app', ['browserSyn'], () => {
   gulp.watch(['public/*.css', '*.html', 'src/*.js', '*.js', 'spec/*.js', 'public/src/*.js'], browserSync.reload);
 });
 gulp.task('default', ['app']);
